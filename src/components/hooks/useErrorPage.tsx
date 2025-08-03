1c01708884b29092077bdf1fc7a7de5d83aa15ec
iNikitina <nikitosik.khv@gmail.com>
2025-08-03 08:43:30 +0300

    Hook commit 1

---
+++ src/components/hooks/useErrorPage.tsx
@@ -0,0 +1,22 @@
+import {characters, defaultHero} from "../../utils/constants.ts";
+import {useParams} from "react-router";
+import {useContext, useEffect} from "react";
+import {SWContext} from "../../utils/context.ts";
+
+
+export  const useErrorPage = () => {
+    const {heroId = defaultHero} = useParams();
+    const {changeHero} = useContext(SWContext);
+
+    useEffect(() => {
+        if (!(heroId in characters)) {
+            return;
+        }
+        changeHero(heroId);
+    }, [heroId])
+
+    return {
+        isError: !(heroId in characters),
+        heroId
+    }
+}
